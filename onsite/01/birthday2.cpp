2ed7b5ea3c5bba7e425e251a7396da1423187b1c
Rachatat Bunpat <82020386+rbunpat@users.noreply.github.com>
2024-03-08 10:18:16 +0700

    its workinggggg

--- onsite/01/birthday2.cpp
+++ onsite/01/birthday2.cpp
@@ -4,39 +4,34 @@ using namespace std;
 
 int main()
 {
-    int sum = 0;
+    int i, j, sum = 0;
     char ch;
 
-    for (int i = 1; i < 32; i *= 2)
+    for (i = 1; i < 32; i *= 2)
     {
-        int co = 0;
+        int co;
+        co = 0;
 
-        for (int j = 1; j < 32; j++)
+        for (j = 1; j < 32; j++)
+
+        if ((j & i) == i)
         {
+            cout << setw(3) << j;
+            co++;
 
-            if (j & i == i)
-            {
-                cout << setw(3) << j;
-                co++;
-
-                if (co % 5 == 0)
-                {
-                    cout << endl;
-                }
-            }
-
-            cout << endl
-                 << "Is your birthday in the table? (y/n): ";
-            cin >> ch;
-
-            if (ch == 'y' || ch == 'Y')
-            {
-                sum += i;
-            }
+            if (co % 5 == 0) cout << endl;
         }
 
-        cout << "Your birthday is on " << sum << endl;
+        cout << endl
+             << "Is your birthday in the table? (y/n): ";
+        cin >> ch;
+
+        if (ch == 'y' || ch == 'Y') sum += i;
+
+
+    }
+
+            cout << "Your birthday is on " << sum << endl;
 
         return 0;
-    }
 }
