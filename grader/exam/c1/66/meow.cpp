d31721f53ac7df5b4f471391e72f7bf634c41573
Rachatat Bunpat <82020386+rbunpat@users.noreply.github.com>
2024-03-13 13:49:24 +0700

    poipoiuopiujj

--- grader/exam/c1/66/meow.cpp
+++ grader/exam/c1/66/meow.cpp
@@ -3,35 +3,62 @@
 using namespace std;
 
 int main() {
-    stringstream output;
+    string checkString = "meow";
+    string input;
 
-    int stringCount;
-    cin >> stringCount >> ws;
+    bool isMeow = true;
+    bool isM = false;
 
-    string input[stringCount];
+    int n, index;
+    cin >> n >> ws;
 
-    for (int i = 0; i < stringCount; i++) {
-        getline(cin, input[i]);
-    }
+    while (n--) {
 
-    for (int i = 0; i < stringCount; i++) {
-        string currentString = input[i];
+        getline(cin, input);
 
-        for (int bruh = 0; bruh < currentString.length(); bruh++) {
-            if (currentString[bruh] == 'm' && currentString[bruh + 1] == 'e' && currentString[bruh + 2] == 'o' && currentString[bruh + 3] == 'w') {
-                if (currentString[bruh + 4] == ' ') {
-                    output << "NO" << endl;
-                } else {
-                    output << "YES" << endl;
+        index = 0;
+        isMeow = true;
+
+        while (index < input.length() && isMeow) {
+            if (input[index]  == ' ') {
+                index += 1;
+                continue;
+            }
+
+            isM = input[index] == 'm';
+
+            while (index < input.length() && input[index] != ' ' && isM && isMeow) {
+                for (int i = 0; i < 4; i++) {
+                    if ((index + i) >= input.length() || input[index + i] != checkString[i]) {
+                        isM = false;
+                        break;
+                    } else {
+                        isM = true;
+                        break;
+                    }
+                }
+
+                if (isM) {
+                    index += 4;
+                    break;
                 }
             }
+
+
+            if (isM) {
+                cout << "YES" << endl;
+                isMeow = false;
+            }
+
+            while (index < input.length() && input[index] != ' ') {
+                index += 1;
+            }  
         }
 
-
+        if (isMeow) {
+            cout << "NO" << endl;
+        }
     }
-
-    cout << output.str();
-
 
 
     return 0;
