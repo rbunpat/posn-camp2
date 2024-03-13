94f0a703d523b360a40306360c31e1bd4311cdb2
Rachatat Bunpat <82020386+rbunpat@users.noreply.github.com>
2024-03-13 13:16:42 +0700

    tidy up folders

---
+++ grader/TOI/01/nugget.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    stringstream output;
+    bool isNugget = false;
+    int n;
+    cin >> n;
+    for (int i = 0; i <= n; i++) {
+        if (i % 6 == 0 || i % 9 == 0 || i % 20 == 0 ) {
+            if (i != 0) {
+                output << i << "\n";
+                isNugget = true;
+            }
+            
+        }
+    }
+
+    if (isNugget == false) {
+        cout << "no";
+    } else {
+        cout << output.str();
+    }
+
+    return 0;
+}
